91f6fec78d8684802374c3edf652fe29b1407249
DylanSantiagoRodriguez <dylan.rodriguez@campusucc.edu.co>
2025-05-13 20:54:59 -0500

    rutas para ligar usuario a traves de id

--- backend/Resolution/AppResolution/urls.py
+++ backend/Resolution/AppResolution/urls.py
@@ -1,22 +1,31 @@
 from django.urls import path
 
-from AppResolution.views import UserView, ClaimView, RequestView, ProfileView, AuthenticationView
+from AppResolution.views import UserView, ClaimView, RequestView, ProfileView, AuthenticationView, LoginView
 
 urlpatterns = [
     # User endpoints
     path('user', UserView.as_view()),
     path('user/<int:pk>', UserView.as_view()),
+    
+    # Login endpoint
+    path('login', LoginView.as_view()),
 
-    #Auth endpoints
+    # Auth endpoints
     path('auth', AuthenticationView.as_view()),
     path('auth/<int:pkid>', AuthenticationView.as_view()),
 
     # Claim endpoints
     path('claim', ClaimView.as_view()),
+    path('claim/<int:pk>', ClaimView.as_view()),
+    path('claim/user/<int:user_id>', ClaimView.as_view()),
     
     # Request endpoints
     path('request', RequestView.as_view()),
+    path('request/<int:pk>', RequestView.as_view()),
+    path('request/user/<int:user_id>', RequestView.as_view()),
     
     # Profile endpoints
     path('profile', ProfileView.as_view()),
+    path('profile/<int:pk>', ProfileView.as_view()),
+    path('profile/user/<int:user_id>', ProfileView.as_view()),
 ]
