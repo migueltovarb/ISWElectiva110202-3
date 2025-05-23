8dd94146ad55954ef9a7e440c1fa71803f5e9a6a
DylanSantiagoRodriguez <dylan.rodriguez@campusucc.edu.co>
2025-05-23 11:03:40 -0500

    models

--- examenSemana16/backend/Examen/AppExamen/models.py
+++ examenSemana16/backend/Examen/AppExamen/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Task(models.Model):
+    todo_list_id = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
